12ac4685e27a2feeeace48cca3357786a3ca79e4
Davi Zappala <94610458+Davizappala@users.noreply.github.com>
2023-03-18 05:40:14 -0300

    Add Files

---
+++ IMC.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso , altura, imc;
+
+            Console.WriteLine("Insira sua altura em cm e usando vírgula: ");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira seu peso: ");
+            peso = double.Parse(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            Console.WriteLine($"Seu IMC é de {imc}");
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Peso abaixo do normal");
+            }
+            else if ((imc >= 18.5) && (imc <= 25))
+            {
+                Console.WriteLine("Peso Normal");
+            }
+            else if ((imc >= 25) && (imc <= 30))
+            {
+                Console.WriteLine("Acima do peso");
+            }
+            else
+            {
+                Console.WriteLine("Obesidade");
+            }
+
+
+        }
+
+
+    }
+}
